15dc5cb827f4e5281677bdbccc4f9b767e0c4a28
Subhra Mondal <30802518+subhramo@users.noreply.github.com>
2020-07-03 09:32:14 +1000

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 825030697311.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin xxxxxxxxx.dkr.ecr.ap-southeast-2.amazonaws.com
 
-docker tag demo:latest 825030697311.dkr.ecr.ap-southeast-2.amazonaws.com/demo:latest
+docker tag demo:latest xxxxxxxxx.dkr.ecr.ap-southeast-2.amazonaws.com/demo:latest
 
-docker push 825030697311.dkr.ecr.ap-southeast-2.amazonaws.com/demo:latest
+docker push xxxxxxxxxxx.dkr.ecr.ap-southeast-2.amazonaws.com/demo:latest
